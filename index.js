bc979f67ed9fb8823279aaab0d9be5513dce723b
mimi46-w <mimilami030@gmail.com>
2025-07-18 13:20:34 +0100

    Create index.js
    ping

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once('ready', () => {
+  console.log('Bot en ligne !');
+});
+
+client.on('messageCreate', message => {
+  if(message.content === '!ping') {
+    message.channel.send('Pong!');
+  }
+});
+
+client.login(process.env.TOKEN);
